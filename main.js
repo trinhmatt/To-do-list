4d1131eb92dcde6ad10884f8c3e7dce7cacd7619
trinhmatt <trinhmatthew@gmail.com>
2017-09-23 13:08:33 -0400

    Added ability to cross out completed tasks

--- main.js
+++ main.js
@@ -2,15 +2,12 @@ function add_task(){
   var task = document.getElementById('task').value
   var check_box = '<input type=\'checkbox\'>'
   if (task !== '') {
-    document.getElementById('tasklist').innerHTML += '<li>'+task+'</li>'
-    document.getElementById('checkbox').innerHTML += check_box
+    document.getElementById('tasklist').innerHTML += '<li>'+check_box+'<span>'+task+'</span>'+'</li>'
     document.getElementById('task').value = ''
   }
 }
 
 function clear_tasks() {
   document.getElementById('tasklist').innerHTML = ''
-  document.getElementById('checkbox').innerHTML = '' 
+  document.getElementById('checkbox').innerHTML = ''
 }
-
-//Need to find out how to position
